df2e54e79eeca529906c580ce7785a6b1ce35fb5
v-t-9 <violeta.a.t.9@gmail.com>
2023-03-08 16:41:43 -0300

    1er commit

---
+++ README.md
@@ -0,0 +1,2 @@
+<h3>Página tributo sobre Nikola Tesla</h3>
+Esta Página fue hecha para practicar lo aprendido sobre HTML, CSS y JavaScript.
---
+++ script.js
@@ -0,0 +1,24 @@
+function visibilityCorrienteAlterna() {
+    document.getElementById("foto-inv-ac").style.visibility = "hidden";
+}
+
+function visibilityCentralHidroelectrica() {
+    document.getElementById("foto-inv-central-hidroelectrica").style.visibility = "hidden";
+}
+
+function visibilityRadio() {
+    document.getElementById("foto-inv-radio").style.visibility = "hidden";
+}
+
+function visibilityBobina() {
+    document.getElementById("foto-inv-bobina").style.visibility = "hidden";
+}
+
+function visibilityLamparaNeon() {
+    document.getElementById("foto-inv-lampara-neon").style.visibility = "hidden";
+}
+
+function visibilityRayosX() {
+    document.getElementById("foto-inv-rayos-x").style.visibility = "hidden";
+}
+
